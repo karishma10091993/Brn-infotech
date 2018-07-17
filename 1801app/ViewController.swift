17b2dc954311c3a0804cb568fa620e9a830cea08
Arun Kumar Reddy <arun.itspectrum@achiever-117.local>
2018-07-17 14:38:01 +0530

    Initial Commit

---
+++ 1801app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1801app
+//
+//  Created by Arun Kumar Reddy on 7/17/18.
+//  Copyright © 2018 Arun Kumar Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
